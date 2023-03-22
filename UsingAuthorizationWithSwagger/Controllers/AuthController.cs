e596b48658bfa73679a7f3ba7c523c240c7ea57e
amerlin <andrea.merlin@aalto.it>
2023-03-22 13:43:57 +0100

    add files

---
+++ UsingAuthorizationWithSwagger/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsingAuthorizationWithSwagger.Models;
+
+namespace UsingAuthorizationWithSwagger.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(); 
+            }
+
+            if (model.UserName == "johndoe" && model.Password == "johndoe2410")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@2410"));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Country, "Italy"));
+                claims.Add(new Claim(ClaimTypes.Name, model.UserName));
+                claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+                claims.Add(new Claim(ClaimTypes.Role, "SuperUser"));
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "CodeMaze",
+                    audience: "https://localhost:5001",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signinCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
